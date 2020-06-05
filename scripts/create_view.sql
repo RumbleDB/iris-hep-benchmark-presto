1cc2187f71a31960fd0118c677c444deb509ff73
DanGraur <dan.graur@inf.ethz.ch>
2020-06-05 14:57:12 +0200

    Changed the structure of the master branch.

---
+++ scripts/create_view.sql
@@ -0,0 +1,152 @@
+DROP TABLE IF EXISTS jet_table;
+DROP TABLE IF EXISTS electron_table;
+DROP TABLE IF EXISTS muon_table;
+
+CREATE TABLE IF NOT EXISTS jet_table AS
+SELECT event, 
+  array_agg(
+    CAST(
+      ROW(
+        pt, 
+        eta, 
+        mass, 
+        puId, 
+        btag
+      ) 
+      AS ROW(
+        pt DOUBLE, 
+        eta DOUBLE, 
+        mass DOUBLE, 
+        puId BOOLEAN, 
+        btag DOUBLE
+      )
+    )
+  ) AS Jet
+FROM memory.cern.run2012b_singlemu_small
+CROSS JOIN UNNEST(Jet_pt, Jet_eta, Jet_mass, Jet_puId, Jet_btag) AS t (pt, eta, mass, puId, btag)
+GROUP BY event;
+
+CREATE TABLE IF NOT EXISTS electron_table AS
+SELECT event,
+  array_agg(
+    CAST(
+      ROW(
+        pt, 
+        eta, 
+        phi, 
+        mass, 
+        charge, 
+        pfRelIso03_all, 
+        dxy, 
+        dxyErr, 
+        dz, 
+        dzErr, 
+        cutBasedId, 
+        pfId, 
+        jetIdx, 
+        genPartIdx
+      )
+      AS ROW(
+        pt DOUBLE, 
+        eta DOUBLE,
+        phi DOUBLE, 
+        mass DOUBLE, 
+        charge INTEGER, 
+        pfRelIso03_all DOUBLE, 
+        dxy DOUBLE, 
+        dxyErr DOUBLE, 
+        dz DOUBLE, 
+        dzErr DOUBLE, 
+        cutBasedId BOOLEAN, 
+        pfId BOOLEAN, 
+        jetIdx INTEGER, 
+        genPartIdx INTEGER
+      )
+    )
+  ) AS Electron
+FROM memory.cern.run2012b_singlemu_small
+CROSS JOIN UNNEST(Electron_pt, Electron_eta, Electron_phi, Electron_mass, Electron_charge, Electron_pfRelIso03_all,
+        Electron_dxy, Electron_dxyErr,  Electron_dz, Electron_dzErr, Electron_cutBasedId, Electron_pfId, Electron_jetIdx,
+        Electron_genPartIdx)
+      AS t (pt, eta, phi, mass, charge, pfRelIso03_all, dxy, dxyErr, dz, dzErr, cutBasedId, pfId, jetIdx, genPartIdx)
+GROUP BY event;
+
+CREATE TABLE IF NOT EXISTS muon_table AS
+SELECT event,
+  array_agg(
+    CAST(
+      ROW(
+        pt, 
+        eta, 
+        phi, 
+        mass, 
+        charge, 
+        pfRelIso03_all, 
+        pfRelIso04_all, 
+        tightId, 
+        softId, 
+        dxy, 
+        dxyErr,
+        dz, 
+        dzErr, 
+        jetIdx, 
+        genPartIdx
+      )
+      AS ROW(
+        pt DOUBLE, 
+        eta DOUBLE, 
+        phi DOUBLE, 
+        mass DOUBLE, 
+        charge INTEGER, 
+        pfRelIso03_all DOUBLE, 
+        pfRelIso04_all DOUBLE, 
+        tightId BOOLEAN, 
+        softId BOOLEAN, 
+        dxy DOUBLE, 
+        dxyErr DOUBLE,
+        dz DOUBLE, 
+        dzErr DOUBLE, 
+        jetIdx INTEGER, 
+        genPartIdx INTEGER
+
+      )
+    )
+  ) AS Muon
+FROM memory.cern.run2012b_singlemu_small
+CROSS JOIN UNNEST(Muon_pt, Muon_eta, Muon_phi, Muon_mass, Muon_charge, Muon_pfRelIso03_all, Muon_pfRelIso04_all,  
+        Muon_tightId, Muon_softId, Muon_dxy, Muon_dxyErr, Muon_dz, Muon_dzErr, Muon_jetIdx, Muon_genPartIdx)
+      AS t (pt, eta, phi, mass, charge, pfRelIso03_all, pfRelIso04_all, tightId, softId, dxy, dxyErr, dz, dzErr, 
+        jetIdx, genPartIdx)
+GROUP BY event;
+
+CREATE OR REPLACE VIEW memory.cern.view AS
+SELECT 
+  main.event, 
+  main.run,
+  main.luminosityBlock,
+  main.HLT_IsoMu24_eta2p,
+  main.HLT_IsoMu24,
+  main.HLT_IsoMu17_eta2p1_LooseIsoPFTau20,
+  main.PV_npvs,
+  main.PV_x,
+  main.PV_y,
+  main.PV_z,
+  main.MET_pt,
+  main.MET_phi,
+  main.MET_sumet,
+  main.MET_significance,
+  main.MET_CovXX,
+  main.MET_CovXY,
+  main.MET_CovYY,
+  main.nTau,
+  main.nPhoton,
+  main.nElectron,
+  Electron AS Electrons,
+  main.nMuon, 
+  Muon AS Muons,
+  main.nJet,
+  Jet AS Jets
+FROM memory.cern.run2012b_singlemu_small AS main
+FULL JOIN jet_table AS j on main.event = j.event
+FULL JOIN electron_table AS e ON main.event = e.event
+FULL JOIN muon_table AS m ON main.event = m.event;
---
+++ scripts/csv_to_sql_insert.py
@@ -0,0 +1,97 @@
+import pandas
+import re
+
+from os.path import join
+from os import getcwd
+from subprocess import run
+
+CSV_PATH = '/home/dan/data/garbage/cern_queries/presto/data/Run2012B_SingleMu_small.parquet'
+TABLE_NAME = "memory.cern.Run2012B_SingleMu_small"
+
+
+def make_sql_insert_script(path, col_name='str'):
+	data = pandas.read_csv(path)	
+	with open("insert_data.sql", "w") as f:
+		f.write("-- Insert data into the `companies` table\nINSERT INTO {} VALUES\n".format(TABLE_NAME))
+
+		for i, row in data.iterrows():
+			f.write(",\n\t(" + row[col_name] + ")" if i != 0 else "\t(" + row[col_name] + ")")
+
+		f.write(";")
+
+def gradually_insert(path, col_name='str', catalog='memory'):
+	"""
+	Gradually inserts the rows in the DF located at path. The rows
+	should already be concatenated together into a string under 
+	the column col_name.
+
+	:param path: the path to the DF
+	:param col_name: the column under which the stringified rows are
+					 located
+	:param catalog: the presto catalog which will be used
+	"""
+	def _execute_command(q):
+		with open("temp.sql", "w") as f:
+			f.write(collector)
+		run(['./run_presto.sh', catalog, "temp.sql"])  
+
+	data = pandas.read_csv(path)
+	collector = cl = "INSERT INTO {} VALUES".format(TABLE_NAME) 
+
+	for i, row in data.iterrows():
+		if i % 300 == 0 and i != 0:
+			print("At row:", i)
+			collector += " (" + row[col_name] + ");"
+			_execute_command(collector)
+			collector = cl 
+		else:
+			collector += " (" + row[col_name] + "),"
+
+	# if there some extra rows that need to be added
+	if collector != cl:
+		collector = collector[:-1] + ";"
+		_execute_command(collector)
+
+
+def stringify_data(path, col_name="str", out_name="../data/stringified.csv"):
+	"""
+	Read a parquet file as a DF, and add a new column whose name is specified  
+	in the col_name parameter. This column holds the row's other columns 
+	under concatenated string format. The new DF is saved under the name 
+	indicated by out_name. 
+
+	:param path: the path to the DF
+	:param col_name: the name of the new column
+	:param out_name: the name of the output csv
+	"""
+	data = pandas.read_parquet(path)
+	data[col_name] = data[data.columns].astype(str).apply(lambda x: ', '.join(x), axis=1)
+	data.to_csv(out_name, index=False)
+
+
+def add_commas(path, col_name="str", out_name="../data/stringified_new.csv"):
+	"""
+	Processes the stringified rows, and makes sure the data is in a format 
+	that presto accepts when inserting.
+
+	:param path: the path to the DF
+	:param col_name: the name of the column with the stringified data 
+	:param out_name: the name of the output csv
+	"""
+	def _inner_transform(x):
+		x = re.sub(' +', ',', x)
+		x = re.sub(",+", ",", x)
+		x = re.sub("\[,", "[", x)
+		x = re.sub(",\]", "]", x)
+		x = re.sub("nan", "null", x)
+		return re.sub("\[", "ARRAY[", x)
+
+	data = pandas.read_csv(path)
+	data[col_name] = data[col_name].apply(_inner_transform)
+	data.to_csv(out_name, index=False)
+
+
+if __name__ == '__main__':
+	# stringify_data(CSV_PATH)
+	# add_commas("../data/stringified.csv")
+	gradually_insert("../data/stringified_new.csv")
