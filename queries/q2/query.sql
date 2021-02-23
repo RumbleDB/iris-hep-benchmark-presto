de685811458873ed6843f1f4d6d397504d21f4ab
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2021-02-23 19:44:45 +0100

    Change Q2 to work with the view (instead of the original data).

--- queries/q2/query.sql
+++ queries/q2/query.sql
@@ -1,17 +1,17 @@
 SELECT
   CAST((
     CASE
-      WHEN jet < 15 THEN 15
-      WHEN jet > 60 THEN 60
-      ELSE jet
+      WHEN jet.pt < 15 THEN 15
+      WHEN jet.pt > 60 THEN 60
+      ELSE jet.pt
     END - 0.225) / 0.45 AS BIGINT) * 0.45 + 0.225 AS x,
   COUNT(*) AS y
 FROM {input_table}
-CROSS JOIN UNNEST(Jet_pt) AS t (jet)
+CROSS JOIN UNNEST(Jets) AS jet
 GROUP BY CAST((
     CASE
-      WHEN jet < 15 THEN 15
-      WHEN jet > 60 THEN 60
-      ELSE jet
+      WHEN jet.pt < 15 THEN 15
+      WHEN jet.pt > 60 THEN 60
+      ELSE jet.pt
     END - 0.225) / 0.45 AS BIGINT) * 0.45 + 0.225
 ORDER BY x;
