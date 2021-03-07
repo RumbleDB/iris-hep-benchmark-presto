5dffc90a7080d3c716a94c7322ee473b883ab9bf
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2021-03-07 16:48:50 +0100

    Q7: Simplify.

--- queries/q7/query.sql
+++ queries/q7/query.sql
@@ -1,43 +1,24 @@
--- UNNEST the Jets and filter out those with pt < 30
-WITH unnested_jets AS (
-  SELECT
-    *,
-    CAST( ROW( j.pt, j.eta, j.phi, j.mass, j.idx ) AS ROW( pt REAL, eta REAL, phi REAL, mass REAL, idx INTEGER ) ) AS j
-  FROM {input_table}
-  CROSS JOIN UNNEST(Jet) WITH ORDINALITY AS j (pt, eta, phi, mass, puId, btag, idx)
-  WHERE j.pt > 30
-),
-
--- Create the tables which hold the jet-other_particle pairs
-filtered_particles AS (
-  SELECT
-    event,
-    j,
-    COALESCE(
-      cardinality(filter(Electron, x -> x.pt > 10 AND sqrt( (j.eta - x.eta) * (j.eta - x.eta) + pow( (j.phi - x.phi + pi()) % (2 * pi()) - pi(), 2) ) < 0.4) ),
-      0
-    ) AS filtered_electron_count,
-    COALESCE(
-      cardinality(filter(Muon, x -> x.pt > 10 AND sqrt( (j.eta - x.eta) * (j.eta - x.eta) + pow( (j.phi - x.phi + pi()) % (2 * pi()) - pi(), 2) ) < 0.4) ),
-      0
-    ) AS filtered_muon_count
-  FROM unnested_jets
-),
-
-
--- Compute the per event jet.pt sums for the remaining jets
-pt_sums AS (
+-- Compute sum of pt of each matching jet
+WITH matching_jets AS (
   SELECT event, SUM(j.pt) AS pt_sum
-  FROM filtered_particles
-  WHERE filtered_electron_count = 0 AND filtered_muon_count = 0
+  FROM {input_table}
+  CROSS JOIN UNNEST(Jet) AS j (pt, eta, phi, mass, puId, btag)
+  WHERE
+    j.pt > 30 AND
+    none_match(Electron,
+               x -> x.pt > 10 AND
+               sqrt( (j.eta - x.eta) * (j.eta - x.eta) +
+                     pow( (j.phi - x.phi + pi()) % (2 * pi()) - pi(), 2) ) < 0.4) AND
+    none_match(Muon,
+               x -> x.pt > 10 AND
+               sqrt( (j.eta - x.eta) * (j.eta - x.eta) +
+                     pow( (j.phi - x.phi + pi()) % (2 * pi()) - pi(), 2) ) < 0.4)
   GROUP BY event
 )
-
-
 -- Compute the histogram
 SELECT
   mysql.default.HistogramBin(pt_sum, 15, 200, 100) AS x,
   COUNT(*) AS y
-FROM pt_sums
+FROM matching_jets
 GROUP BY mysql.default.HistogramBin(pt_sum, 15, 200, 100)
 ORDER BY x;
