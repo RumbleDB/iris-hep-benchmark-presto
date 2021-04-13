ecd6c8f3a897d4ee1078635df0bae122fc6478d9
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2021-04-13 19:38:26 +0200

    Plot pt instead of sumet (as suggested by Mason).

--- queries/query-1/query.sql
+++ queries/query-1/query.sql
@@ -1,6 +1,6 @@
 SELECT
-  mysql.default.HistogramBin(MET.sumet, 0, 2000, 100) AS x,
+  mysql.default.HistogramBin(MET.pt, 0, 2000, 100) AS x,
   COUNT(*) AS y
 FROM {input_table}
-GROUP BY mysql.default.HistogramBin(MET.sumet, 0, 2000, 100)
+GROUP BY mysql.default.HistogramBin(MET.pt, 0, 2000, 100)
 ORDER BY x;
--- queries/query-5/query.sql
+++ queries/query-5/query.sql
@@ -1,5 +1,5 @@
 WITH temp AS (
-  SELECT event, MET.sumet, COUNT(*)
+  SELECT event, MET.pt, COUNT(*)
   FROM {input_table}
   CROSS JOIN UNNEST(Muon) WITH ORDINALITY
     AS m1 (pt, eta, phi, mass, charge, pfRelIso03_all, pfRelIso04_all, tightId,
@@ -12,12 +12,12 @@ WITH temp AS (
     m1.idx < m2.idx AND
     m1.charge <> m2.charge AND
     2 * m1.pt * m2.pt * (COSH(m1.eta - m2.eta) - COS(m1.phi - m2.phi)) BETWEEN 60 AND 120
-  GROUP BY event, MET.sumet
+  GROUP BY event, MET.pt
   HAVING COUNT(*) > 0
 )
 SELECT
-  mysql.default.HistogramBin(sumet, 0, 2000, 100) AS x,
+  mysql.default.HistogramBin(pt, 0, 2000, 100) AS x,
   COUNT(*) AS y
 FROM temp
-GROUP BY mysql.default.HistogramBin(sumet, 0, 2000, 100)
+GROUP BY mysql.default.HistogramBin(pt, 0, 2000, 100)
 ORDER BY x;
